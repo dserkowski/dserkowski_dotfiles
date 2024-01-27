43beb7d0dc66552ab0e93e7fbc1a83053df6d4b1
Daniel Serkowski <dserkowski@atlassian.com>
2024-01-27 01:44:39 +0100

    add App Store installer cli - mas

--- macos/update.sh
+++ macos/update.sh
@@ -7,6 +7,7 @@ set -x
 brew update && brew upgrade && brew cleanup
 commandExists omz && omz update  
 commandExists nvm && nvm install 'lts/*'
+commandExists mas && mas upgrade
 # softwareupdate --install --recommended --background
 # pyenv update # TODO
 pip-review --local --auto
